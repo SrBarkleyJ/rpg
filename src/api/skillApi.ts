be2be866cc6b149ae809fcc5891628b2d155f4b6
SrBarkleyJ <jorgexdddd@gmail.com>
2025-12-05 10:51:57 +0100

    Delete src directory

--- src/api/skillApi.ts
+++
@@ -1,8 +0,0 @@
-import apiClient from './apiClient';
-
-const skillApi = {
-    getSkills: () => apiClient.get('/skills'),
-    upgradeSkill: (skillId: string) => apiClient.post('/skills/upgrade', { skillId }),
-};
-
-export default skillApi;
